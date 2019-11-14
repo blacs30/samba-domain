87c657621e3f066a4817f91c5e14610d1d43ff1d
blacs30 <github@lisowski-development.com>
2019-11-14 17:33:24 +0100

    Add armv7 dockerfile.

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@
 USER=blacs30
 NAME=samba-domain
 
-for arch in amd64 arm64v8; do
-    docker build --no-cache -f Dockerfile.${arch} -t $USER/$NAME:${arch}-18.04 -t $USER/$NAME:${arch}-latest .
-    docker push $USER/$NAME:${arch}-latest
-    docker push $USER/$NAME:${arch}-18.04
+for arch in armv7 amd64; do
+    # docker build --no-cache -f Dockerfile.${arch} -t $USER/$NAME:${arch}-18.04 -t $USER/$NAME:${arch}-latest .
+    docker buildx build -f Dockerfile.${arch} --push -t $USER/$NAME:${arch}-18.04 -t $USER/$NAME:${arch}-latest   .
 done
